3f6375b2e82f1ee46d528b191ea4ea77a4ed62c4
Zeeshan Mukhtar <Zeshanmukhtar878@gmail.com>
2023-09-01 10:15:50 -0700

    first route

--- Back__End/index.js
+++ Back__End/index.js
@@ -3,6 +3,11 @@ import { PORT } from './config.js';
 
 const app = express();
 
+// making 1st route at /
+app.get('/', (req, res) => {
+  res.json('route is working');
+  return res.status(200);
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
