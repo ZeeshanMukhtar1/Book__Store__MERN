ee554d422d762602766864d221fc968c1ae564a0
Zeeshan Mukhtar <Zeshanmukhtar878@gmail.com>
2023-09-03 03:45:12 -0700

    Created post request and post some books

--- Back__End/index.js
+++ Back__End/index.js
@@ -1,25 +1,46 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotEnv from 'dotenv';
-dotEnv.config();
+import dotenv from 'dotenv';
+import { Book } from './Models/Books__Model.js';
+
+dotenv.config();
 
 const app = express();
 
 // Define constants first
 const PORT = process.env.PORT || 5000;
 
-// making 1st route at /
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Root route at /
 app.get('/', (req, res) => {
-  res.json('route is working');
+  res.json('Route is working');
+});
+
+// Route for saving a new book (Create_Operation)
+app.post('/books', async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send('Please fill all the fields');
+    } else {
+      const newBook = await Book.create(req.body);
+      return res.status(200).send(newBook);
+    }
+  } catch (error) {
+    console.error('Error', error);
+    res.status(500).send('Server error');
+  }
 });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
 const databaseUrl = process.env.DATABASE_URL;
 console.log('DATABASE_URL', databaseUrl);
 
-// connecting to MongoDB
+// Connecting to MongoDB
 mongoose
   .connect(databaseUrl, {
     useNewUrlParser: true,
