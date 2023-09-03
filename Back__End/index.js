ddf37f2f4f46cbadd2b2cfea57d32fbae92b72d2
Zeeshan Mukhtar <Zeshanmukhtar878@gmail.com>
2023-09-03 08:05:44 -0700

    added cors policy

--- Back__End/index.js
+++ Back__End/index.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 import { Book } from './Models/Books__Model.js';
 //  importing book router
 import booksRouter from './Routes/Books__Route.js';
+// importing cors pkg for corss origin resource sharing in browser
+import cors from 'cors';
 
 dotenv.config();
 
@@ -15,12 +17,24 @@ const PORT = process.env.PORT || 5000;
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
+// applying cors policy
+//  default option => it will alllow all origins *
+app.use(cors());
+//  2nd option in whiuch we allow custom origins , we have more control on it now
+// app.use(
+//   cors({
+//     origin: 'http://localhost:5000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Methods should be enclosed in quotes
+//     allowedHeaders: ['content-type'],
+//   })
+// );
+
 // Root route at /
 app.get('/', (req, res) => {
   res.json('Route is working');
 });
 
-// Using middleware for the /books route
+// Using middleware for the /books prefix
 app.use('/books', booksRouter);
 
 app.listen(PORT, () => {
